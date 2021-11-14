f95671e41547eb902173434c5dd6d48c4f30358a
TimHHatcher <thatcher@docutap.com>
2021-11-14 16:22:29 -0600

    Banking test update

--- cypress/integration/examples/bankTests.spec.js
+++ cypress/integration/examples/bankTests.spec.js
@@ -2,12 +2,17 @@ describe('Bank tests', () => {
 	beforeEach('Load the website', () => {
 		cy.visit('http://zero.webappsecurity.com/')
 		cy.url().should('contain', 'zero.webappsecurity.com')
+		cy.title().should(
+			'contain',
+			'Zero - Personal Banking - Loans - Credit Cards'
+		)
 	})
 
 	it('Login', () => {
 		cy.get('#signin_button').click()
 		cy.get('#user_login').type('SomeLogin')
 		cy.get('#user_password').type('SomePassword')
+		cy.get('#user_remember_me').click()
 		cy.get('input').contains('Sign in').click()
 		cy.get('.alert-error')
 			.should('be.visible')
